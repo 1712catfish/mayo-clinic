e5195dbbb529bc50fae809ea9de2ffad36fca063
tan-kun <1712huynhtan@gmail.com>
2022-08-18 12:10:37 +0700

    Push

--- train/train.py
+++ train/train.py
@@ -3,8 +3,12 @@ try:
 except Exception:
     from setup_train import *
 
-history = mcsai_nt_model.fit(train_ds, validation_data=val_ds,
-                             epochs=EPOCHS, callbacks=callbacks,
+history = mcsai_nt_model.fit(train_ds,
+                             steps_per_epoch=STEPS_PER_EPOCH,
+                             validation_data=val_ds,
+                             validation_steps=VALIDATION_STEPS,
+                             epochs=EPOCHS,
+                             callbacks=callbacks,
                              verbose=VERBOSE,
                              class_weight=CLASS_WEIGHT)
 plot_history(history, metrics=('acc', 'auc'))
