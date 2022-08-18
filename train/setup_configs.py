b500587f2fc865b18c7fdabf5b993bebfcad1ca1
tan-kun <1712huynhtan@gmail.com>
2022-08-18 12:35:23 +0700

    Push

--- train/setup_configs.py
+++ train/setup_configs.py
@@ -3,16 +3,17 @@ try:
 except Exception:
     from setup_device import *
 
-INPUT_PATH = Path('/kaggle/input/mayo-clinic-strip-ai')
-IMAGE_DIR = Path('/kaggle/input/jpg-images-strip-ai')
-
-CSV_GCS = 'gs://kds-5b35e8caec4c36d580aef4332f9e7811adbc4591990a3a47a73c3fe0'
 IMAGE_GCS = 'gs://kds-dcf15753e60a6c61a37238f890ada2818c287fd8745815b311985bc8'
 
-TRAIN_DIR = INPUT_PATH / 'train'
-TRAIN_CSV = INPUT_PATH / 'train.csv'
-TEST_DIR = INPUT_PATH / 'test'
-TEST_CSV = INPUT_PATH / 'test.csv'
+CSV_PATH = '/kaggle/input/mayo-clinic-strip-ai'
+# IMAGE_PATH = '/kaggle/input/jpg-images-strip-ai'
+IMAGE_PATH = IMAGE_GCS
+
+TRAIN_CSV = os.path.join(CSV_PATH, 'train.csv')
+TEST_CSV = os.path.join(CSV_PATH, 'test.csv')
+
+TRAIN_DIR = os.path.join(IMAGE_PATH, 'train')
+TEST_DIR = os.path.join(IMAGE_PATH, 'test')
 
 IMAGE_SHAPE = (512, 512, 3)
 S2I_LBL_MAP = {'CE': 0, 'LAA': 1}
@@ -22,10 +23,11 @@ VERBOSE = 1
 
 if TPU is not None:
     BATCH_SIZE = 32 * STRATEGY.num_replicas_in_sync
-    IMAGE_DIR = IMAGE_GCS
+    # IMAGE_DIR = IMAGE_GCS
 else:
     BATCH_SIZE = 4
-    IMAGE_DIR = INPUT_PATH
+    # IMAGE_DIR = INPUT_PATH
 
 print('Batch size:', BATCH_SIZE)
-print('IMAGE_DIR:', IMAGE_DIR)
+print('TRAIN_DIR:', TRAIN_DIR)
+print('TRAIN_CSV:', TRAIN_CSV)
--- train/setup_data.py
+++ train/setup_data.py
@@ -5,7 +5,7 @@ except Exception:
     from build_utils.data_utils import *
 
 df = pd.read_csv(TRAIN_CSV)
-df["image_path"] = df["image_id"].apply(lambda x: os.path.join(IMAGE_DIR, "train", x + ".jpg"))
+df["image_path"] = df["image_id"].apply(lambda x: os.path.join(TRAIN_DIR, x + ".jpg"))
 train_df, val_df = k_fold_train_test_split(df)
 
 CLASS_WEIGHT = auto_class_weight(train_df.label)
@@ -20,7 +20,7 @@ val_ds = create_dataset(val_df, ordered=True,
                         cached=False, repeated=False, augmented=False)
 
 test_df = pd.read_csv(TEST_CSV)
-test_df["image_path"] = test_df["image_id"].apply(lambda x: os.path.join(IMAGE_DIR, "test", x + ".jpg"))
+test_df["image_path"] = test_df["image_id"].apply(lambda x: os.path.join(TEST_DIR, x + ".jpg"))
 
 N_TRAIN, N_VAL, N_TEST = len(train_df), len(val_df), len(test_df)
 STEPS_PER_EPOCH = N_TRAIN // BATCH_SIZE
@@ -29,4 +29,6 @@ VALIDATION_STEPS = N_VAL // BATCH_SIZE
 print(f'Found {N_TRAIN} in train_ds')
 print(f'Found {N_VAL} in val_ds')
 print(f'FOUND {N_TEST} in test_ds')
+print(f'STEPS_PER_EPOCH:', STEPS_PER_EPOCH)
+print(f'VALIDATION_STEPS:', VALIDATION_STEPS)
 
